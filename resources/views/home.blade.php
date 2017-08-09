bb0cbbed72480098df6aff80c84b10131151acde
Your Name <you@example.com>
2017-08-08 21:22:34 -0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+      @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+      @endif
+      @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+      @endif
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
