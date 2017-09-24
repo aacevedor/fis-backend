cbef4d42b8bbbb1916168136d445eec3ff71e7df
aacevedoday <you@example.com>
2017-09-24 16:05:33 -0500

    Delete file seeder services

--- app/Services.php
+++ app/Services.php
@@ -9,6 +9,6 @@ use App\Services\ServicesTypes;
 class Services extends Model
 {
     public function service_type(){
-      return $this->belongsTo(ServicesTypes::class);
+      return $this->hasOne(ServicesTypes::class);
     }
 }
