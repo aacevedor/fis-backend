81bab72b73e333d7eeef778c2218ba027a514577
aacevedoday <you@example.com>
2017-10-09 00:33:56 -0500

    created method update ionic

---
+++ database/migrations/2017_10_09_044959_add_field_to_users_table_field_ionic_id.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldToUsersTableFieldIonicId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+          Schema::table('users', function (Blueprint $table) {
+                $table->string('ionic_id')->after('email')->nullable();
+          });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('ionic_id');
+      });
+    }
+}
